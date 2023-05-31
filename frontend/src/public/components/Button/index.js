69980d61ede998fce26f93123d8da062451c6e9d
antoniobfm <toinho111@gmail.com>
2023-05-31 15:18:40 -0300

    Commented Button

--- frontend/src/public/components/Button/index.js
+++ frontend/src/public/components/Button/index.js
@@ -1,17 +1,22 @@
-// Button component
+// Define a custom element called "Button"
 class Button extends HTMLElement {
     constructor() {
+        // Call the parent constructor
         super();
 
+        // Create a shadow DOM for the element
         const shadow = this.attachShadow({ mode: 'open' });
 
+        // Create a button element
         const button = document.createElement('button');
         button.classList.add('button');
 
+        // Create a label element and set its text content to the "label" attribute
         const label = document.createElement('span');
         label.classList.add('label');
         label.textContent = this.getAttribute('label');
 
+        // Create an icon element and set its text content to the "icon" attribute
         const icon = document.createElement('span');
         icon.classList.add('icon');
         icon.textContent = this.getAttribute('icon');
@@ -22,11 +27,14 @@ class Button extends HTMLElement {
         link.href = './components/Button/styles.css';
         shadow.appendChild(link);
 
+        // Add the label and icon elements to the button element
         button.appendChild(label);
         button.appendChild(icon);
 
+        // Add the button element to the shadow DOM
         shadow.appendChild(button);
     }
 }
-  
-customElements.define('dendem-button', Button);
+
+// Register the custom element with the browser
+customElements.define('my-button', Button);
